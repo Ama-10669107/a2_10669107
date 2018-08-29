f4123aef00d9c29f5e988fdd942186b0c4e8fbe8
Ama-10669107 <42800138+Ama-10669107@users.noreply.github.com>
2018-08-29 21:38:03 +0000

    Add files via upload

---
+++ a2_10669107.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	int x;
+	cout<<"enter number"<<endl;
+	cin>>"x";
+	while (x>2);
+	cout<<"prime number"
+	;cin>>"x"
+	for(x = 2;;);
+	2/1 ==2 
+	;2/2==1
+	;int a;
+	cout<<"positive integer"
+	cin>>"a"
+	while(a>0);
+	bool}x/a==float // true
+	
+		//return 0;
+//}
